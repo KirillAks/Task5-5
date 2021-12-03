29427baa763e686cd6f57466b972ca3c80947fbc
Кирилл Аксенов <kirill.ax@tprofeng.ru>
2021-12-03 12:12:11 +0300

    Корректировка кода

--- Task5-5/Program.cs
+++ Task5-5/Program.cs
@@ -13,8 +13,17 @@ namespace Task5_5
         {
             Console.WriteLine("Введите целое число");
             int n = Convert.ToInt32(Console.ReadLine());
-            int[,] twoDimensionalArray = new int[n, n] { { 1, 0, 1, 0, 1 }, { 0, 1, 0, 1, 0 } };
-
+            int[,] twoDimensionalArray = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    twoDimensionalArray[i, j] = 1 - (i + j) % 2;
+                    Console.Write("{0,2}", twoDimensionalArray[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
