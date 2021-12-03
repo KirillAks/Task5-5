b8d8e57755dea5189ac1e3865d1f1570f87835de
Кирилл Аксенов <kirill.ax@tprofeng.ru>
2021-12-03 11:22:38 +0300

    Добавьте файлы проекта.

---
+++ Task5-5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_5
+{
+    class Program
+    {
+        // Запросить у пользователя целочисленное значение N. Сформировать двумерный массив размера NxN следующего вида: 1 0 1 0 1, 0 1 0 1 0
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] twoDimensionalArray = new int[n, n] { { 1, 0, 1, 0, 1 }, { 0, 1, 0, 1, 0 } };
+
+        }
+    }
+}
